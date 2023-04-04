9bbe8a93beea6a5827fd87f0b52e58c92e0a0931
Vladyslavua <vladgershman@gmail.com>
2023-04-04 19:50:20 +0200

    update: refactoring store from redux to reduxToolkit

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,21 +1,34 @@
 import { tasksReducer } from './tasks-reducer';
 import { todolistsReducer } from './todolists-reducer';
 import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux';
-import thunkMiddleware, {ThunkAction} from 'redux-thunk'
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
+import {authReducer} from "features/auth-reducer";
+import {appReducer} from "app/app-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
+
 const rootReducer = combineReducers({
     tasks: tasksReducer,
-    todolists: todolistsReducer
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer
 })
-// непосредственно создаём store
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
-// определить автоматически тип всего объекта состояния
+
+
+
+export const store = configureStore({
+    reducer:rootReducer
+});
+
+
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+
+
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+
 // @ts-ignore
 window.store = store;
